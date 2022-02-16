d031f764a9409933b91d473b51737601bc661c58
a5an0 <207713+a5an0@users.noreply.github.com>
2022-02-15 23:00:52 -0500

    working on setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "Installing wget, qrencode, gpg, and npm"
+apt update
+
 apt install -y wget qrencode gpg npm
 
-echo "installing markdown-pdf"
-npm install -g markdown-pdf
+npm install -g markdown-pdf --ignore-scripts
 
 wget https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-x86_64-linux-gnu.tar.gz
 tar xvzf bitcoin-22.0-x86_64-linux-gnu.tar.gz
 chmod +x bitcoin-22.0/bin/*
 mv bitcoin-22.0/bin/{bitcoind,bitcoin-cli} /usr/local/bin/
 
+rm -rf bitcoin-22.0*
