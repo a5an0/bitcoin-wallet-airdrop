dadfe00535e9ad7c178f9fa84ac335f80473bbc6
a5an0 <207713+a5an0@users.noreply.github.com>
2022-02-16 17:46:11 -0500

    check signatures on bitcoin core

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -9,8 +9,23 @@ sudo apt install -y gconf-service libgbm-dev libasound2 libatk1.0-0 libc6 libcai
 # install pdf rendering package
 sudo npm install -g mdpdf --unsafe-perm=true -allow-root
 
-# get necessary bitcoin-core components. TODO: check sigs on download
+# get necessary bitcoin-core components.
 wget https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-x86_64-linux-gnu.tar.gz
+wget https://bitcoincore.org/bin/bitcoin-core-22.0/SHA256SUMS
+wget https://bitcoincore.org/bin/bitcoin-core-22.0/SHA256SUMS.asc
+sha256sum --ignore-missing --check SHA256SUMS | grep "bitcoin-22.0-x86_64-linux-gnu.tar.gz: OK"
+if [ $? -ne 0 ]; then
+  echo "CHECKSUM FAILED ON BITCOIN CORE DOWNLOAD"
+  exit 1
+fi
+wget https://raw.githubusercontent.com/bitcoin/bitcoin/master/contrib/builder-keys/keys.txt
+while read fingerprint keyholder_name; do gpg --keyserver hkps://keys.openpgp.org --recv-keys ${fingerprint}; done < ./keys.txt
+gpg --verify SHA256SUMS.asc 2>&1 | grep "Good signature"
+if [ $? -ne 0 ]; then
+  echo "COULD NOT VERIFY SIGNATURES ON RELEASE"
+  exit 1
+fi
+
 tar xvzf bitcoin-22.0-x86_64-linux-gnu.tar.gz
 chmod +x bitcoin-22.0/bin/*
 sudo mv bitcoin-22.0/bin/{bitcoind,bitcoin-cli} /usr/local/bin/
